1b509c1d60673842163d3d7dceb6422c0522fe67
myyrakle <sssang97@naver.com>
2023-06-06 22:45:16 +0900

    add: post_one

--- new_route_with_crud/router.rs
+++ new_route_with_crud/router.rs
@@ -13,18 +13,13 @@ use axum::{
 use super::service::#Name#Service;
 
 use super::dtos::create_#name#_request::Create#Name#Request;
-use super::dtos::create_#name#_response::Create#Name#Response;
-use super::dtos::get_#name#_response::Get#Name#Response;
-use super::dtos::list_#name#_response::List#Name#Response;
 use super::dtos::update_#name#_request::Update#Name#Request;
-use super::dtos::update_#name#_response::Update#Name#Response;
-use super::dtos::delete_#name#_response::Delete#Name#Response;
 
 pub(crate) async fn get_router() -> Router {
     let router = Router::new()
         .route("/", get(get_list))
         .route("/:id", get(get_one))
-        .route("/:id", post(health))
+        .route("/:id", post(post_one))
         .route("/:id", put(health))
         .route("/:id", delete(health))
         .layer(trace);
@@ -45,6 +40,14 @@ async fn get_one(Path(id): Path<i32>) -> impl IntoResponse {
     let service = #Name#Service::new();
 
     let response = service.find_one(id);
+
+    Json(response).into_response()
+}
+
+async fn post_one(Json(body): Json<Create#Name#Request>) -> impl IntoResponse {
+    let service = #Name#Service::new();
+
+    let response = service.create_one(id);
 
     Json(response).into_response()
 }
